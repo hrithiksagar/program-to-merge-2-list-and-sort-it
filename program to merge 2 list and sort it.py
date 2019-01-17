347b98a93a5f3c1c8f95a684d96286253dbfd485
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2019-01-17 21:30:24 +0530

    Add files via upload

---
+++ program to merge 2 list and sort it.py
@@ -0,0 +1,14 @@
+#program to merge 2 list and sort it
+l1=[]
+l2=[]
+s1=int(input("enter the number of elements of first list"))
+for i in range(s1):
+    x=int(input("enter list items"))  
+    l2.append(x)
+print(l1)
+
+s2=int(input("enter the number of elements of second list"))
+for i in range(s2):
+    y=int(input("enter list items"))  
+    l2.append(y)  
+print(l2)
